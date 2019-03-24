a841bff7f093fbd38e86deaaac79ec0b7a81f256
Alyssa Brown <alyssa.brown@live.com>
2019-03-24 16:28:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,26 +9,26 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Cat Adoption", "Pets", 650, "3-2-19", "3-17-19");
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES ("Meals on Wheels", "Homeless", 800, "6-5-19", "6-6-19");
 
-INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), 
-INSERT INTO users (id, name, age) VALUES (2, 'Bear', 6), 
-INSERT INTO users (id, name, age) VALUES INSERT INTO users (id, name, age) VALUES (3, 'Iguana', 4), 
-INSERT INTO users (id, name, age) VALUES (4, 'Alex', 33),
-INSERT INTO users (id, name, age) VALUES (5, 'Amanda', 24), 
-INSERT INTO users (id, name, age) VALUES (6, 'Sophie', 24), 
-INSERT INTO users (id, name, age) VALUES (7, 'Rosey', 9), 
-INSERT INTO users (id, name, age) VALUES (8, 'Victoria', 23), 
-INSERT INTO users (id, name, age) VALUES (9, 'Franz', 100), 
-INSERT INTO users (id, name, age) VALUES (10, 'Hermione', 30),
-INSERT INTO users (id, name, age) VALUES (11, 'Voldemort', 90), 
-INSERT INTO users (id, name, age) VALUES (12, 'Marisa', 24), 
-INSERT INTO users (id, name, age) VALUES (13, 'Swizzle', 4), 
-INSERT INTO users (id, name, age) VALUES (14, 'Sirius', 36), 
-INSERT INTO users (id, name, age) VALUES (15, 'Albus', 113), 
-INSERT INTO users (id, name, age) VALUES (16, 'Squid', 5),
-INSERT INTO users (id, name, age) VALUES (17, 'Whale', 6), 
-INSERT INTO users (id, name, age) VALUES (18, 'Pacha', 5), 
-INSERT INTO users (id, name, age) VALUES (19, 'Ena', 24), 
-(20, 'Katie', 24);
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17); 
+INSERT INTO users (id, name, age) VALUES (2, 'Bear', 6); 
+INSERT INTO users (id, name, age) VALUES (3, 'Iguana', 4);
+INSERT INTO users (id, name, age) VALUES (4, 'Alex', 33);
+INSERT INTO users (id, name, age) VALUES (5, 'Amanda', 24);
+INSERT INTO users (id, name, age) VALUES (6, 'Sophie', 24);
+INSERT INTO users (id, name, age) VALUES (7, 'Rosey', 9);
+INSERT INTO users (id, name, age) VALUES (8, 'Victoria', 23);
+INSERT INTO users (id, name, age) VALUES (9, 'Franz', 100); 
+INSERT INTO users (id, name, age) VALUES (10, 'Hermione', 30);
+INSERT INTO users (id, name, age) VALUES (11, 'Voldemort', 90); 
+INSERT INTO users (id, name, age) VALUES (12, 'Marisa', 24); 
+INSERT INTO users (id, name, age) VALUES (13, 'Swizzle', 4); 
+INSERT INTO users (id, name, age) VALUES (14, 'Sirius', 36); 
+INSERT INTO users (id, name, age) VALUES (15, 'Albus', 113); 
+INSERT INTO users (id, name, age) VALUES (16, 'Squid', 5);
+INSERT INTO users (id, name, age) VALUES (17, 'Whale', 6); 
+INSERT INTO users (id, name, age) VALUES (18, 'Pacha', 5); 
+INSERT INTO users (id, name, age) VALUES (19, 'Ena', 24); 
+INSERT INTO users (id, name, age) VALUES (20, 'Katie', 24);
 
 INSERT INTO pledges (amount, user_id, project_id) VALUES (65, 1, 2);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (30, 1, 6);
