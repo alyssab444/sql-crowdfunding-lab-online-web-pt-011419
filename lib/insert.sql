7d4254711b2bf603bccd43e1b50caca275b2e51d
Alyssa Brown <alyssa.brown@live.com>
2019-03-24 15:44:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO projects (id, title, category, funding_goal, net_worth) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
